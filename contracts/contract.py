0b4c6b6b29e687fd6c59248da1954e8a7d0be51b
nojus <nojus9999@gmail.com>
2023-03-16 20:51:18 +0200

    git commit

--- contracts/contract.py
+++ contracts/contract.py
@@ -1,9 +1,22 @@
 from pyteal import *
+import program 
 
 
 
 def approval():
-    return Approve()
+    global_counter = Bytes("counter") # uint64
+    global_owner = Bytes("owner") # bytes 
+
+
+    return program.event(
+        init=Seq(
+            # create a variable
+            App.globalPut(global_counter, Int(0)),
+            App.globalPut(global_owner, Bytes("Me")),
+            Approve()
+            
+        )
+    )
 
 def clear():
     return Approve()
